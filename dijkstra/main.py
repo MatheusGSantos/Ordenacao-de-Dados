8de4f292e03d4adada240a74275fe409eb0e6379
Matheus Gama dos Santos <36708181+MatheusGSantos@users.noreply.github.com>
2019-11-13 18:21:12 -0300

    Add files via upload

--- dijkstra/main.py
+++ dijkstra/main.py
@@ -1,12 +1,5 @@
 import sys
-import copy
-
-# criando a classe Heap
-class Heap:
-    def __init__(self, heap_ar):
-        self.heap_array = heap_ar
-        self.heap_size = len(heap_ar)
-
+import heapq
 
 # criando a classe Grafo
 class Graph:
@@ -22,6 +15,25 @@ class Graph:
     def add_edge(self, vertex1, vertex2, weight):
         self.adj_list[vertex1][vertex2] = weight
         self.adj_list[vertex2][vertex1] = weight
+
+
+    def dijkstra(self, start, finish):
+        queue = []
+        distances = {}
+        previous = {}
+        # initialize
+        for vertex in self.adj_list:
+            if(vertex == start):
+                distances[vertex] = 0
+                heapq.heappush(queue, [0, vertex])
+            else:
+                distances[vertex] = sys.maxsize
+                heapq.heappush(queue, [sys.maxsize, vertex])
+            previous[vertex] = None
+
+        # enquanto tiver elementos na fila
+        while queue:
+            pass
 
 
 
@@ -43,9 +55,9 @@ with open("dij10.txt", 'r') as input_f:
         for j in range(len(matrix[i])):
             matrix[i][j] = int(matrix[i][j])
 
-
-    #matrix.append([])
     """
+    matrix.append([])
+    
     # adicionar 0 e rebater o triangulo
     auxiliar = 0
     i = 0
@@ -58,8 +70,8 @@ with open("dij10.txt", 'r') as input_f:
         i += 1
         auxiliar += 1
     """
-    for p in matrix:
-        print(p)
+    """for p in matrix:
+        print(p)"""
     # agora temos uma matriz de vértices
 
     ## criando o grafo e inserindo vertices e arestas
@@ -75,109 +87,11 @@ with open("dij10.txt", 'r') as input_f:
             grafo.add_edge(x,y,matrix[x][aux])
             aux += 1
         aux = 0
-    
 """
     for each_k in grafo.adj_list:
         print(each_k)
         print(grafo.adj_list[each_k])
 """    
 
-
-
-# função que retorna o indice do filho da esquerda
-def left(i):
-    if i == 0:
-        return i+1
-    else:
-        return int(2*i)
-
-
-# função que retorna o filho da direita
-def right(i):
-    if i == 0:
-        return i+2
-    else:
-        return int(2*i + 1)
-
-
-# função que retorna o pai
-def parent(i):
-    return int((i-1)/2)
-
-
-# função que realiza a operação de troca do max heap numa "subarvore" da heap
-def min_heapify(i):
-    global m_heap
-    l = left(i)
-    r = right(i)
-    menor = -1  # assumindo que a heap só contenha números positivos
-    if (l <= m_heap.heap_size - 1) and (m_heap.heap_array[l] < m_heap.heap_array[i]):
-        menor = l
-    else:
-        menor = i
-    if (r <= m_heap.heap_size - 1) and m_heap.heap_array[r] < m_heap.heap_array[menor]:
-        menor = r
-    if menor != i:
-        aux = m_heap.heap_array[i]
-        m_heap.heap_array[i] = m_heap.heap_array[menor]
-        m_heap.heap_array[menor] = aux
-        min_heapify(menor)
-
-
-# função que aplica max_heapify em toda a heap
-def build_min_heap():
-    global m_heap
-    m_heap.heap_size = len(m_heap.heap_array)
-    i = int(m_heap.heap_size / 2)
-    for i in range(i-1,-1,-1):
-        min_heapify(i)
-
-
-# retorna o valor minimo (raiz)
-def heap_minimum():
-    global m_heap
-    return m_heap.heap_array
-
-
-# extrai o valor mínimo
-def heap_extract_min():
-    if(m_heap.heap_size < 1):
-        return "heap underflow"
-    minimo = m_heap.heap_array[0]
-    m_heap.heap_array[0] = m_heap.heap_array[m_heap.heap_size-1]
-    m_heap.heap_size -= 1
-    min_heapify(0)
-    return minimo
-
-
-# sobe o nó
-def heap_decrease_key(i, key): #key: valor inteiro a ser colocado
-    global m_heap
-    if(key > m_heap.heap_array[i]):
-        return "nada a ser feito"
-    m_heap.heap_array[i] = key
-    while (i>1) and (m_heap.heap_array[parent(i)] < m_heap.heap_array[i]):
-        aux = m_heap.heap_array[i]
-        m_heap.heap_array[i] = m_heap.heap_array[parent(i)]
-        m_heap.heap_array[parent(i)] = aux
-        i = parent(i)
-
-
-# insere uma chave
-def min_heap_insert(key):
-    global m_heap
-    m_heap.heap_array += 1
-    m_heap.heap_array.append(sys.maxsize)
-    heap_decrease_key(m_heap.heap_size,key)
-
-
-def func():
-    return
-
-
 # main
-"""print("Array antes:")
-print(m_heap.heap_array)
-build_min_heap()
-print("\nArray ordenado:")
-print(m_heap.heap_array)"""
+grafo.dijkstra(0,9)
