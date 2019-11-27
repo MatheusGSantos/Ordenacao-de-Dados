bfc86d319452ee6a81d99fd8ab53d6704d1e4414
Matheus Gama dos Santos <36708181+MatheusGSantos@users.noreply.github.com>
2019-11-26 21:48:40 -0300

    Add files via upload

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -1,22 +1,27 @@
+/* C++ Libraries*/
 #include <iostream>
 #include <queue>
 #include <fstream>
-#include <locale>
 #include <tuple>
-#include <cstdlib>
 #include <vector>
-#include <string>
+
+/* C Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
 using namespace std;
+typedef unsigned char byte;
 
+//tree node
 class node{
     public:
         unsigned frequency;
-        wchar_t character;
+        byte character;
         node* left;
         node* right;
         
-        node(int f, wchar_t c){
+        node(int f, byte c){
             frequency = f;
             character = c;
             left = NULL;
@@ -27,26 +32,22 @@ class node{
 };
 
 
-
 //writing a character into a file
 void write_byte(ofstream file, char c)
 {
     file.put(c);
 }
 
+
+/* Compression */
 /*
-
-Compression
-
-*/
-
 node buildHuffmanTree(unsigned bytes[256])
 {
     // priority queue < frequencia, identificador >
     priority_queue< 
-        tuple <unsigned, wchar_t>, 
-        vector< tuple <unsigned, wchar_t> >, 
-        greater<tuple <unsigned, wchar_t> > 
+        tuple <unsigned, byte>, 
+        vector< tuple <unsigned, byte> >, 
+        greater<tuple <unsigned, byte> > 
         > pq;
 
     // dando enqueue
@@ -54,35 +55,49 @@ node buildHuffmanTree(unsigned bytes[256])
     {
         if(bytes[i] != 0)
         {
-            wchar_t c;
+            byte c;
             pq.push( make_tuple( bytes[i], c) );
         }
     }
     
     //return node();
 }
-
+*/
 
 
 
 int main() {
     int operation = 0;
-    // read UNICODE
-    locale::global(std::locale("")); // activate user-preferred locale
-
+    setlocale(LC_ALL,"");
     if(operation == 0) //compression
     {
-        wifstream wf("test.txt", ios::in | ios::binary); // test.txt contains utf-8 text
+        FILE* input_f = fopen("test.txt", "rb"); // test.txt contains utf-8 text
         //get every byte frequency
-        wchar_t c;
+        byte c;
         unsigned bytes[256] = {0};
-        while(wf.get(c))
-            bytes[c]+= 1;
-        
+        int temp;
+        //char c2;
+        /*while(*/fread(&c, sizeof(byte), 1, input_f);//)
+        //{
+            temp = c;
+        //    bytes[temp]+= 1;
+            printf("%d\n", temp);
+        //}
+        FILE* output_f = fopen("test_out.txt", "wb");
+        fwrite(&c, 1, sizeof(c), output_f);
+/*      
+        for(int i =0; i<256; i++)
+        {
+            if(bytes[i] != 0)
+            {
+                cout << i << endl;
+            }
+        }
+*/        
         //buildHuffmanTree(bytes);
         //node tree = buildHuffmanTree(wf);
     }
     
     //std::ofstream output;
-
+    return 0;
 }
