0e6b3410cea5c4e5c8bf5292f83412efe4c9f714
Matheus Gama dos Santos <36708181+MatheusGSantos@users.noreply.github.com>
2019-12-03 15:27:18 -0300

    Add files via upload

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -17,7 +17,7 @@ using namespace std;
 class node{
     public:
         unsigned frequency;
-        char character;
+        unsigned char character;
         node* left;
         node* right;
         
@@ -50,7 +50,6 @@ node* buildHuffmanTree(unsigned bytes[256])
             n->character = i;
             n->left = NULL;
             n->right = NULL;
-            //cout << "enqueueing: " << i << endl;
             pq.push(n);
         }
     }
@@ -64,7 +63,6 @@ node* buildHuffmanTree(unsigned bytes[256])
         z->left = pq.top(); pq.pop();
         z->right = pq.top(); pq.pop();
         z->frequency = z->left->frequency + z->right->frequency;
-        //cout << z->frequency << " : " << z->character << ", left: { " << z->left->character << " : " << z->left->frequency << "}, right: { " << z->right->character<< " : " << z->right->frequency << "}"<< endl;
         pq.push(z);
     }
     
@@ -102,9 +100,9 @@ void getCodes(node* n, string (&c)[256], string buf)
 
 
 // returns the byte equivalent to the byte sequence (bit manipulation)
-char getByte(char buf[8])
+unsigned char getByte(unsigned char buf[8])
 {
-    char byte = 0;
+    unsigned char byte = 0;
     for(int i = 0; i < 8; i++)
     {
         if (buf[7-i] == '1')
@@ -118,18 +116,19 @@ char getByte(char buf[8])
 
 int main() {
     int operation = 0;
-
     if(operation == 0) // compression
     {
         ifstream in("teste.bin", ios::in | ios::binary);
 
         /* get every byte frequency */
         unsigned bytes[256] = {0};
-        char c;
-        while( in.get(c) )
-        {
+        unsigned char c;
+        while( in.peek() != EOF ){
+            c = in.get();
             bytes[c]+= 1;
+            //cout << c << " : " << bytes[c] << endl;
         }
+        
         
         /* build the huffman tree */
         node* tree = buildHuffmanTree(bytes);
@@ -146,24 +145,28 @@ int main() {
         /* write byte frequency array into output file */
         FILE* fp_out = fopen("teste_c.bin", "wb");
         fwrite(&bytes, sizeof(bytes), 1, fp_out);
-        fclose(fp_out);
+        //fclose(fp_out);
 
         /* start compression */
-        ofstream out("teste_c.bin", ios::app | ios::binary);
-        char byte_buffer[8];
+        //ofstream out("teste_c.bin", ios::app | ios::binary);
+        unsigned char byte_buffer[8];
         int buf_index = 0;
-        while (in.get(c))
+        
+        while ( in.peek() != EOF )
         {
+            c = in.get();
+            //cout << c << " read from file\n";
             int code_size = codes[c].size(); // huffman code size of the byte read
             for(int i = 0; i < code_size; i++)
             {
-                byte_buffer[buf_index++] = codes[c][i]; 
+                byte_buffer[buf_index++] = codes[c][i];
+                cout << codes[c][i] << " bit evaluated\n";
                 if(buf_index == 8)
                 {
                     buf_index = 0;
                     // write byte
-                    char w_byte = getByte(byte_buffer);
-                    out.write(&w_byte,1);
+                    unsigned char w_byte = getByte(byte_buffer);
+                    fwrite(&w_byte, sizeof(w_byte), 1, fp_out);
                 }
             }
         }
@@ -174,11 +177,12 @@ int main() {
         {
             buf_index = 0;
             // write byte
-            char w_byte = getByte(byte_buffer);
-            out.write(&w_byte,1);
+            unsigned char w_byte = getByte(byte_buffer);
+            fwrite(&w_byte, sizeof(w_byte), 1, fp_out);
         }
         in.close();
-        out.close();
+        fclose(fp_out);
+        //out.close();
 
         cout << "Finished compression.\n";
     }
@@ -200,15 +204,36 @@ int main() {
         node* tree = buildHuffmanTree(bytes);
 
         /* start decompression */
-        char byte;
-        while(in.get(byte))
+        unsigned char r_byte;
+        ofstream out("teste_d.bin", ios::out | ios::binary);
+        
+        while(in.peek() != EOF)
         {
-            char direction = 128;
+            r_byte = in.get();
+            unsigned char comparator = 128;
+            node* aux = tree;
+
             for(int i = 0; i < 8; i++)
             {
-
+                if(!(aux->left) && !(aux->right))
+                {
+                    aux = tree;
+                    out.put(aux->character);
+                    cout << aux->character << " in the file\n";
+                }
+                if(r_byte & (comparator >> i) ) // bit = 1, direita
+                {
+                    aux = aux->right;
+                }
+                else    //bit = 0
+                {
+                    aux = aux->left;
+                }
             }
         }
+        in.close();
+        out.close();
+        
     }
     
     
