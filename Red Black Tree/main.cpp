b02ff095c5d8e8f37848e1871c04d1f1edbeab46
Matheus Gama dos Santos <36708181+MatheusGSantos@users.noreply.github.com>
2020-03-24 17:40:34 -0300

    Update main.cpp

--- Red Black Tree/main.cpp
+++ Red Black Tree/main.cpp
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX 5
+#define MAX 100000
 
 using namespace std;
 
-//ok
 struct Node{
     char color; //r ou b
     int value;
@@ -16,7 +15,7 @@ struct Node{
 };
 
 
-void setColor(struct Node* n, char color) //ok
+void setColor(struct Node* n, char color)
 {
     if(n == NULL)
         return;
@@ -25,14 +24,23 @@ void setColor(struct Node* n, char color) //ok
 }
 
 
-void SwapColor(struct Node* a, struct Node* b) //ok
+char getColor(struct Node*n)
+{
+    if(!n)
+        return 'b';
+    return n->color;
+}
+
+
+void SwapColor(struct Node* a, struct Node* b)
 {
     char aux = a->color;
     setColor(a, b->color);
     setColor(b, aux);
 }
 
-struct Node* new_node(int v) //ok
+
+struct Node* new_node(int v)
 {
     struct Node* n = new struct Node;
     n->parent = NULL;
@@ -44,24 +52,24 @@ struct Node* new_node(int v) //ok
 }
 
 
-struct Node* GetParent(struct Node* n) //ok
+struct Node* GetParent(struct Node* n)
 {
     return n == NULL ? NULL : n->parent; //NULL para raiz e vazia, pai para os demais
 }
 
 
-struct Node* GetGrandParent(struct Node* n) //ok
+struct Node* GetGrandParent(struct Node* n)
 {
     return GetParent(GetParent(n)); //retorna avô do nó
 }
 
 
-void treeInsert(struct Node *&n, struct Node* n_node) //ok
+void treeInsert(struct Node *&n, struct Node* n_node)
 {
     if(!n)
     {
         n = n_node; //settando a raiz da árvore
-        cout << "Root is " << n_node->value << endl;
+        //cout << "Root is " << n_node->value << endl;
     }
     else
     {
@@ -72,14 +80,14 @@ void treeInsert(struct Node *&n, struct Node* n_node) //ok
             {
                 if( aux->right )
                 {
-                    cout << "Went to the right\n";
+                    //cout << "Went to the right\n";
                     aux = aux->right;
                 }
                 else
                 {
                     aux->right = n_node;
                     n_node->parent = aux;
-                    cout << "Added " << n_node->value << " to the right of " << n_node->parent->value << endl;
+                    //cout << "Added " << n_node->value << " to the right of " << n_node->parent->value << endl;
                     break;
                 }
             }
@@ -87,14 +95,14 @@ void treeInsert(struct Node *&n, struct Node* n_node) //ok
             {
                 if( aux->left )
                 {
-                    cout << "Went to the left\n";
+                    //cout << "Went to the left\n";
                     aux = aux->left;
                 }
                 else
                 {
                     aux->left = n_node;
                     n_node->parent = aux;
-                    cout << "Added " << n_node->value << " to the left of " << n_node->parent->value << endl;
+                    //cout << "Added " << n_node->value << " to the left of " << n_node->parent->value << endl;
                     break;
                 }
             }
@@ -152,13 +160,13 @@ void InsertRepair(struct Node *&root, struct Node *&n)
 {
     struct Node *parent = NULL;
     struct Node *grandparent = NULL;
-    while ( (n != root) && n->color == 'r' && n->parent->color == 'r')
+    while ( (n != root) && getColor(n) == 'r' && getColor(n->parent) == 'r')
     {
         parent = GetParent(n);
         grandparent = GetGrandParent(n);
         if (parent == grandparent->left) {
             struct Node *uncle = grandparent->right;
-            if (uncle->color == 'r') {
+            if (getColor(uncle) == 'r') {
                 setColor(uncle, 'b');
                 setColor(parent, 'b');
                 setColor(grandparent, 'r');
@@ -175,7 +183,7 @@ void InsertRepair(struct Node *&root, struct Node *&n)
             }
         } else {
             struct Node *uncle = grandparent->left;
-            if (uncle->color == 'r') {
+            if (getColor(uncle) == 'r') {
                 setColor(uncle, 'b');
                 setColor(parent, 'b');
                 setColor(grandparent, 'r');
@@ -198,28 +206,64 @@ void InsertRepair(struct Node *&root, struct Node *&n)
 
 void RBTree_Insert(struct Node *&n, struct Node *&n_node)
 {
-    cout << "Insertion Process Start\n";
+    //cout << "Insertion Process Start\n";
     treeInsert(n, n_node);
     InsertRepair(n, n_node);
-    cout << "Node color: "<< n_node->color << endl;
+    //cout << "Node color: "<< n_node->color << endl;
+}
+
+
+struct Node* Search(struct Node* root, int v)
+{
+    struct Node* aux = root;
+    while(aux)
+    {
+        if(aux->value == v)
+            return aux;
+        else if(v > aux->value)
+            aux = aux->right;
+        else
+            aux = aux->left;
+    }
+    return aux;
+}
+
+
+void Search_RBT(struct Node* root, int v)
+{
+    struct Node* found = Search(root, v);
+    if(found)
+    {
+        cout <<  "Description\n{\nColor: " << found->color << endl;
+        cout << "Value: " << found->value << endl;
+        cout << "Parent: " << (found->parent == NULL ? -1: found->parent->value) << endl;
+        cout << "Left: " << (found->left == NULL ? -1: found->left->value) << endl;
+        cout << "Right: " << (found->right == NULL ? -1: found->right->value) << "\n}\n\n";
+    }
+    else
+        cout << "Value not found: " << v << "\n\n";
 }
 
 
 int main() {
     srand (time(NULL));
     int rand_ar[MAX];
+    cout << "Setting up the " << MAX << " element(s) array..." << endl;
     for(int i = 0; i<MAX; i++)
     {
         rand_ar[i] = rand() % MAX+30+1;
-        cout << rand_ar[i] << endl;
+        //cout << rand_ar[i] << endl;
     }
 
     struct Node *root = NULL;
+    cout << "Inserting nodes...\n";
     for(int i = 0; i<MAX; i++)
     {
         struct Node* n = new_node(rand_ar[i]);
         RBTree_Insert(root, n);
     }
-
+    cout << "Finished inserting nodes\n\n";
+    for(int j=0; j < MAX+30+1; j+=20000)
+        Search_RBT(root, j);
     return 0;
 }
